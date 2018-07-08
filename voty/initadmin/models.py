9e6e2d49790cb0015e685f423329df7a86643b90
frequent <svenfranck@yahoo.com>
2018-07-08 19:09:12 +0200

    cleanup files

--- voty/initadmin/models.py
+++ voty/initadmin/models.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# Voty initadmin models
+# ==============================================================================
+#
+# parameters (*default)
+# ------------------------------------------------------------------------------
 from django.contrib.auth.models import User
 from datetime import datetime, timedelta, date
 from django.db import models
+
 import pytz
 
+# ----------------------------- InviteBatch ------------------------------------
 class InviteBatch(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
 	total_found = models.IntegerField(default=0)
 	new_added = models.IntegerField(default=0)
 	payload = models.TextField()
 
-
-
+# ----------------------------- UserConfig -------------------------------------
+# XXX should team lead be set here instead of in a separate group?
 class UserConfig(models.Model):
     user = models.OneToOneField(User, related_name="config", on_delete=models.CASCADE)
     is_diverse_mod = models.BooleanField(default=False)
--- voty/initadmin/notify_backend.py
+++ voty/initadmin/notify_backend.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# Voty initadmin notification (Backend handling)
+# ==============================================================================
+#
+# parameters (*default)
+# ------------------------------------------------------------------------------
 from django.utils.translation import ugettext
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
@@ -5,36 +12,34 @@ from django.db.models import Q
 from pinax.notifications.backends.base import BaseBackend
 from notifications.signals import notify
 
-
-class SiteBackend(BaseBackend):
-    spam_sensitivity = 0
-    
-    def deliver(self, recipient, sender, notice_type, extra_context):
-        notify_kw = {
-          "verb": notice_type.label
-        }
-        for x in ['action_object', 'target', 'verb', 'description']:
-          if x in extra_context:
-            notify_kw[x] = extra_context[x]
-
-        notify.send(sender, recipient=recipient, **notify_kw)
-
-
-
+# mark all notifications related to initiative passed in request as read
 def mark_as_read(get_response):
-    """
-    Mark all notifications related to the initiative of the given request
-    as read
-    """
-    def middleware(request):
-        # from voty.initproc.models import Initiative
-        response = get_response(request)
-        if request.user and request.user.is_authenticated and getattr(request, 'initiative', None):
-          init_type = ContentType.objects.get_for_model(request.initiative)
-          q = request.user.notifications.filter(
-              Q(actor_content_type=init_type, actor_object_id=request.initiative.id) | \
-              Q(target_content_type=init_type, target_object_id=request.initiative.id)).mark_all_as_read()
+  def middleware(request):
 
-        return response
+      # XXX ? # from voty.initproc.models import Initiative
+      response = get_response(request)
+      if request.user and request.user.is_authenticated and getattr(request, "initiative", None):
+        initiative_type = ContentType.objects.get_for_model(request.initiative)
+        q = request.user.notifications.filter(
+          Q(actor_content_type=initiative_type, actor_object_id=request.initiative.id) | \
+          Q(target_content_type=initiative_type, target_object_id=request.initiative.id)
+        ).mark_all_as_read()
 
-    return middleware
+      return response
+
+  return middleware
+
+# ---------------------------- SiteBackend -------------------------------------
+class SiteBackend(BaseBackend):
+
+  # XXX what's this for?
+  spam_sensitivity = 0
+
+  def deliver(self, recipient, sender, notice_type, extra_context):
+    notify_kw = {"verb": notice_type.label}
+    for x in ['action_object', 'target', 'verb', 'description']:
+      if x in extra_context:
+        notify_kw[x] = extra_context[x]
+
+    # XXX collustion with Initivative notify?
+    notify.send(sender, recipient=recipient, **notify_kw)
