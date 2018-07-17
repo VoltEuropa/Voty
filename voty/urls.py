3afff5ad80e0e51a70b7c6c9cffb9a1aa32a9268
frequent <svenfranck@yahoo.com>
2018-07-17 14:28:55 +0200

    move notification urls to initadmin

--- voty/urls.py
+++ voty/urls.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # ==============================================================================
 # Voty GLOBAL Url Configuration
 # ==============================================================================
@@ -22,7 +21,6 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
-import notifications.urls
 from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 
@@ -35,8 +33,6 @@ urlpatterns += i18n_patterns(
   url(r"^about", TemplateView.as_view(template_name="static/about.html")),
   url(r"^help", TemplateView.as_view(template_name="static/help.html")),
   url(r"^register", TemplateView.as_view(template_name="static/register.html")),
-  url(r"^messages/", include(notifications.urls, namespace="notifications")),
-  url(r"^messages/", include("pinax.notifications.urls")),
   url(r"", include("voty.initproc.urls")),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
