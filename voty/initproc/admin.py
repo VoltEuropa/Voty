b770fc30c3ff41c20ce3679963ace581797a9b7c
frequent <svenfranck@yahoo.com>
2018-07-30 21:56:00 +0200

    file cleanup

--- voty/initproc/admin.py
+++ voty/initproc/admin.py
@@ -1,27 +1,37 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# Voty initproc Admin
+# ==============================================================================
+#
+# parameters (*default)
+# ------------------------------------------------------------------------------
+
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
 from .models import Initiative, Quorum, Supporter, Pro, Contra, Proposal, Comment, Vote, Moderation
 
+# --------------------------- InitiativeAdmin ----------------------------------
 class InitiativeAdmin(VersionAdmin):
-    list_display = ['title', 'state', 'created_at', 'changed_at']
-    ordering = ['title', 'created_at', 'changed_at']
-    # actions = ['move_on', 'send_invite', 'decline']
-    search_fields = ['title', 'summary']
+  list_display = ['title', 'state', 'created_at', 'changed_at']
+  ordering = ['title', 'created_at', 'changed_at']
 
+  # actions = ['move_on', 'send_invite', 'decline']
+  search_fields = ['title', 'summary']
 
+# ----------------------------- SupportAdmin --.--------------------------------
 class SupporterAdmin(admin.ModelAdmin):
-	list_display = ['created_at', 'initiative', 'user', 'initiator', 'first', 'ack']
-	ordering = ['created_at']
+  list_display = ['created_at', 'initiative', 'user', 'initiator', 'first', 'ack']
+  ordering = ['created_at']
 
-	search_fields = ['initiative__title', 'user__username', 'user__first_name', 'user__last_name']
+  search_fields = ['initiative__title', 'user__username', 'user__first_name', 'user__last_name']
 
-
+# --------------------------- ModerationAdmin ----------------------------------
 class ModerationAdmin(admin.ModelAdmin):
-	list_display = ['created_at', 'initiative', 'user', 'vote', 'stale']
-	ordering = ['created_at']
+  list_display = ['created_at', 'initiative', 'user', 'vote', 'stale']
+  ordering = ['created_at']
 
-	search_fields = ['initiative__title', 'user__username', 'user__first_name', 'user__last_name']
+  search_fields = ['initiative__title', 'user__username', 'user__first_name', 'user__last_name']
 
 
 admin.site.register(Initiative, InitiativeAdmin)
@@ -33,3 +43,4 @@ admin.site.register(Proposal)
 admin.site.register(Comment)
 admin.site.register(Moderation, ModerationAdmin)
 admin.site.register(Vote)
+
--- voty/initproc/apps.py
+++ voty/initproc/apps.py
@@ -1,5 +1,18 @@
+# -*- coding: utf-8 -*-
+# ==============================================================================
+# Voty initproc App
+# ==============================================================================
+#
+# parameters (*default)
+# ------------------------------------------------------------------------------
+
 from django.apps import AppConfig
 
+# --------------------------- Initproc Config ----------------------------------
 class InitprocConfig(AppConfig):
-    name = "voty.initproc"
-    verbose_name = "Initiative Process"
+
+  # XXX does this make initproc models importable?
+  name = "voty.initproc"
+  verbose_name = "Initiative Process"
+
+
